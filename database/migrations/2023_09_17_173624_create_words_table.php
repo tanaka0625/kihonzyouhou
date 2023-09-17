27790bcb6d385ab92f53ca9ea9303fab9eda2a70
tanaka0625 <tanasou6@gmail.com>
2023-09-18 03:06:38 +0900

    wordsテーブル作成

--- database/migrations/2023_09_17_173624_create_words_table.php
+++ database/migrations/2023_09_17_173624_create_words_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateWordsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('words', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string('word');
+            $table->string('text');
+            $table->string('category');
+            $table->string('sub_category');
+            $table->integer('question_count')->default(0);;
+            $table->integer('mistake_count')->default(0);
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('words');
     }
 }
